14b3d15cb0e28b9238d9ec9ad2223f8b79851c5b
Nicorost2 <nicorostro15@gmail.com>
2025-03-10 19:37:21 -0700

    agregada la funcion despedir

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 # main.py
 def saludar(nombre):
     print(f"Hola, {nombre}!")
+
+def despedir(nombre):
+    print(f"Adiós, {nombre}!")  # Nueva función
+
 if __name__ == "__main__":
     saludar("Mundo")
+    despedir("Mundo")  # Llamada a la nueva función
