6613f55b3760fd9789538be4f829175b18611f2c
Nicorost2 <nicorostro15@gmail.com>
2025-03-10 18:02:26 -0700

    Primer commit: agregados main.py y README.md

---
+++ README.md
@@ -0,0 +1,2 @@
+# Mi Proyecto Python
+Este es un proyecto de ejemplo para practicar Git.
---
+++ main.py
@@ -0,0 +1,5 @@
+# main.py
+def saludar(nombre):
+    print(f"Hola, {nombre}!")
+if __name__ == "__main__":
+    saludar("Mundo")
